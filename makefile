769c89105e0f75975d580dfe091ccfc9dbaf7f8f
Zachkp <zachkordaspotter@gmail.com>
2025-08-05 10:42:21 -0700

    update email.go

--- makefile
+++ makefile
@@ -1,3 +1,2 @@
 run:
 	go run main.go
-
---
+++ models/email.go
@@ -0,0 +1,43 @@
+package models
+
+import (
+	"sync"
+	"time"
+)
+
+type ImapSmtpStore struct {
+	ImapHost string
+	ImapPort string
+	SmtpHost string
+	SmtpPort string
+	Username string
+	Password string
+}
+
+// Constructor function
+func NewImapSmtpStore(username, password, imapHost, imapPort, smtpHost, smtpPort string) *ImapSmtpStore {
+	return &ImapSmtpStore{
+		Username: username,
+		Password: password,
+		ImapHost: imapHost,
+		ImapPort: imapPort,
+		SmtpHost: smtpHost,
+		SmtpPort: smtpPort,
+	}
+}
+
+type Email struct {
+	ID        string
+	From      string
+	To        string
+	Subject   string
+	Body      string
+	Timestamp time.Time
+	Read      bool
+}
+
+type FileStore struct {
+	path   string
+	emails []Email
+	mu     sync.Mutex
+}
